b084d4a04f69afd64ba6f74fdc04c752faf3729e
Qomtec <qomtecsol@gmail.com>
2018-05-23 07:03:54 -0600

    22_05_2018

--- src/pages/about/about.ts
+++ src/pages/about/about.ts
@@ -1,14 +1,18 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
+import { SettingsPage } from '../settings/settings';
 
 @Component({
   selector: 'page-about',
   templateUrl: 'about.html'
 })
 export class AboutPage {
-
+  cards: any;
+  category: string = 'gear';
   constructor(public navCtrl: NavController) {
-
+    this.cards = new Array(10);
   }
-
+  goToAccount(){
+    this.navCtrl.push(SettingsPage);
+  }
 }
